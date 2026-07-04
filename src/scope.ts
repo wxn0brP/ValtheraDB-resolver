222160f08e92107a47692ff39956d1e1c2d97dac
wxn0brP <toja.filiper@gmail.com>
2026-07-04 18:11:05 +0200

    feat: improve ScopeBase

---
+++ src/scope.ts
@@ -0,0 +1,31 @@
+// @ts-ignore
+import { DbDirOpts } from "@wxn0brp/db-storage-dir/types";
+// @ts-ignore
+import { Options as BinOpts } from "@wxn0brp/db-storage-bin";
+// @ts-ignore
+import { MongoClientOptions } from "mongodb";
+// @ts-ignore
+import { RocksOpenOptions } from "@wxn0brp/db-storage-rocks/types";
+// @ts-ignore
+import { EncryptedActionOptions } from "@wxn0brp/db-storage-crypt/crypto";
+// @ts-ignore
+import { RemoteConfig } from "@wxn0brp/db-client/remote";
+// @ts-ignore
+import { Opts as LengthOpts } from "@wxn0brp/db-storage-length/action";
+
+export interface ScopeBase {
+    dir(folder: string, opts?: DbDirOpts): any;
+    bin(path: string, opts?: Partial<BinOpts>): any;
+    sqlite: {
+        (file: string, keys?: Record<string, string>, opts?: any): any;
+        bun(file: string, keys?: Record<string, string>, opts?: any): any;
+        node(file: string, keys?: Record<string, string>, opts?: any): any;
+        better(file: string, keys?: Record<string, string>, opts?: any): any;
+    };
+    mongodb(uri: string, dbName: string, opts?: MongoClientOptions): any;
+    rocks(location: string, opts?: RocksOpenOptions): any;
+    crypt(folder: string, options?: EncryptedActionOptions): any;
+    client(url: string | RemoteConfig): any;
+    accdb(file: string, keys?: Record<string, string>): any;
+    length(opts: LengthOpts): any;
+}
--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,7 @@ import { ValtheraClass } from "@wxn0brp/db-core";
 import { Collection } from "@wxn0brp/db-core/helpers/collection";
 import type { ActionsBaseInterface } from "@wxn0brp/db-core/types/action";
 import { Data } from "@wxn0brp/db-core/types/data";
+import { ScopeBase } from "./scope";
 
 export interface Opts {
     name?: string;
@@ -21,23 +22,6 @@ export type Adapter = (...args: any[]) => Promise<ActionsBaseInterface> | Action
 export type ValtheraLazy<T extends Record<string, Data> = {}> = ValtheraClass & {
     [K in keyof T]: Collection<T[K]>;
 };
-
-export interface ScopeBase {
-    dir(folder: string, opts?: any): any;
-    sqlite: {
-        (file: string, keys?: any, opts?: any): any;
-        bun(file: string, keys?: any, opts?: any): any;
-        node(file: string, keys?: any, opts?: any): any;
-        better(file: string, keys?: any, opts?: any): any;
-    };
-    mongodb(uri: string, dbName: string, opts?: any): any;
-    rocks(location: string, opts?: any): any;
-    crypt(folder: string, options?: any): any;
-    client(url: any, ...args: any[]): any;
-    accdb(file: string, keys?: any, opts?: any): any;
-    length(opts: any): any;
-    bin(path: string, opts?: any): any;
-}
 
 export type _ReplaceReturn<T, S extends Record<string, Data>> =
     T extends (...args: infer A) => any
